a9883a35f465e3f65973496e26747fb8f47fd547
Ismael Cerezo <i.cerezo@actongroup.com>
2025-03-17 11:37:23 +0100

    feat: ajout des contrôleurs, des routes et du middleware d'authentification pour la gestion des utilisateurs, des rooms et des messages

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -1,21 +1,17 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: "postgres",
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 const testConnection = async () => {
   try {
